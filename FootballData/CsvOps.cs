f0d3dc2e50c93049d39cd896d8a69940461b735f
Gary Anderson <gary@garandsoftware.co.uk>
2018-01-09 00:22:02 +0000

    Created a class to parse the CSV data and a mapping file to control how the CSV relates to the Match object

---
+++ FootballData/CsvOps.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CsvHelper;
+using System.IO;
+
+namespace FootballData
+{
+    class CsvOps
+    {
+        internal static List<Match> GetMatchesFromCsv(Stream data)
+        {
+            data.Position = 0;
+            var output = new List<Match>();
+
+            using (var streamReader = new StreamReader(data))
+            {
+                using (var csv = new CsvReader(streamReader))
+                {
+                    csv.Configuration.HasHeaderRecord = true;
+                    csv.Configuration.HeaderValidated = null;
+                    csv.Configuration.MissingFieldFound = null;
+                    csv.Configuration.BadDataFound = null;
+
+                    // Register the class map, found within DataFileMap.cs
+                    csv.Configuration.RegisterClassMap<MatchMap>();
+
+                    while (csv.Read())
+                    {
+                        var record = csv.GetRecord<Match>();
+
+                        // We only want matches from leagues that are in the configuration
+                        var league = Leagues.GetAll().FirstOrDefault(div => div.Id == record.Division.Id);
+
+                        if (league != null)
+                        {
+                            // Can't figure out how to lookup and assign an object in ClassMap so will do it here
+                            record.Division = league;
+
+                            output.Add(record);
+                        }
+                    }
+
+                    return output;
+                }
+            }
+        }
+    }
+}
---
+++ FootballData/DataFileMap.cs
@@ -0,0 +1,69 @@
+﻿using System.Linq;
+using CsvHelper.Configuration;
+
+namespace FootballData
+{
+    internal class MatchMap : ClassMap<Match>
+    {
+        public MatchMap()
+        {
+            Map(x => x.MatchDate).Name("Date");
+            References<LeagueMap>(x => x.Division);
+            References<HomeTeamMap>(x => x.HomeTeam);
+            References<AwayTeamMap>(x => x.AwayTeam);
+            References<RefereeMap>(x => x.Referee);
+            References<FullTimeScoreMap>(x => x.FullTimeScore);
+            References<HalfTimeScoreMap>(x => x.HalfTimeScore);
+        }
+    }
+
+    internal class LeagueMap : ClassMap<League>
+    {
+        public LeagueMap()
+        {
+            Map(x => x.Id).Name("Div");
+        }
+    }
+
+    internal class HomeTeamMap : ClassMap<Team>
+    {
+        public HomeTeamMap()
+        {
+            Map(x => x.Name).Name("HomeTeam");
+        }
+    }
+
+    internal class AwayTeamMap : ClassMap<Team>
+    {
+        public AwayTeamMap()
+        {
+            Map(x => x.Name).Name("AwayTeam");
+        }
+    }
+
+    internal class RefereeMap : ClassMap<Referee>
+    {
+        public RefereeMap()
+        {
+            Map(x => x.Name).Name("Referee");
+        }
+    }
+
+    internal class FullTimeScoreMap : ClassMap<Score>
+    {
+        public FullTimeScoreMap()
+        {
+            Map(x => x.HomeGoals).Name("FTHG").Default(0);
+            Map(x => x.AwayGoals).Name("FTAG").Default(0);
+        }
+    }
+
+    internal class HalfTimeScoreMap : ClassMap<Score>
+    {
+        public HalfTimeScoreMap()
+        {
+            Map(x => x.HomeGoals).Name("HTHG").Default(0);
+            Map(x => x.AwayGoals).Name("HTAG").Default(0);
+        }
+    }
+}
