4891e2a9919502cf53e41b96b4e1e9f27338af94
Gary Anderson <gary@garandsoftware.co.uk>
2018-01-15 11:24:27 +0000

    Added StatType enum

--- FootballData/Statistic.cs
+++ FootballData/Statistic.cs
@@ -2,7 +2,20 @@
 {
     public class Statistic
     {
-        public Statistics.StatType Type { get; set; }
+        public StatType Type { get; set; }
         public int? Value { get; set; }
+
+        public enum StatType
+        {
+            Shots,
+            ShotsOnTarget,
+            HitWoodwork,
+            Corners,
+            FoulsCommitted,
+            Offsides,
+            YellowCards,
+            RedCards,
+            BookingPoints
+        }
     }
 }
