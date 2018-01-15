5ada13034a1d64a377e98ebb86235290bbd16fde
Gary Anderson <gary@garandsoftware.co.uk>
2018-01-15 10:45:42 +0000

    Addition of the Season class to show available seasons

---
+++ FootballData/Season.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FootballData
+{
+    public class Season
+    {
+        public string Id { get; }
+        public string Name { get; }
+        public string ShortName { get; }
+        public string LongName { get; }
+
+        internal int StartYear { get; }
+
+        internal Season(int startYear, int endYear)
+        {
+            // Things get easier with DateTimes
+            var startDate = new DateTime(startYear, 01, 01);
+            var endDate = new DateTime(endYear, 01, 01);
+
+            Id = startDate.ToString("yy") + endDate.ToString("yy");
+
+            Name = String.Format("{0}/{1}", startDate.ToString("yyyy"), endDate.ToString("yy"));
+            ShortName = String.Format("{0}/{1}", startDate.ToString("yy"), endDate.ToString("yy"));
+            LongName = String.Format("{0}/{1}", startDate.ToString("yyyy"), endDate.ToString("yyyy"));
+
+            StartYear = startYear;
+        }
+    }
+}
---
+++ FootballData/Seasons.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballData
+{
+    public static class Seasons
+    {
+        // We'll generate seasons on the fly here. Seasons could go in the config 
+        // file but that would mean changing that once a year forever more. The 
+        // earliest data is for 93/94, so we'll generate them back to then.
+
+        // At the time of writing this code (during 17/18 season) the earliest 
+        // result record for this season was on 28/07. We'll therefore generate a
+        // new season object from July and that will hopefully be suitable forever.
+        // Cue a strange-timed world cup ruining this idea.
+        internal static int StartYear = 1993;
+
+        internal static int LatestSeasonStartYear
+        {
+            get
+            {
+                // var now = new DateTime(2018, 07, 01);
+                var now = DateTime.Now;
+
+                // From January to July the start year of the latest season was the previous year
+                return now.Year - (now.Month < 7 ? 1 : 0);
+            }
+        }
+
+        public static List<Season> Get()
+        {
+            var seasons = new List<Season>();
+            var loopYear = StartYear;
+
+            do
+            {
+                seasons.Add(new Season(loopYear, loopYear + 1));
+
+                loopYear += 1;
+            } while (loopYear <= LatestSeasonStartYear);
+
+            return seasons.OrderByDescending(x => x.StartYear).ToList();
+        }
+    }
+}
