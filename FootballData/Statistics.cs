a10b574c9808a0b91ab6dc5897ea7db8b90d175d
Gary Anderson <gary@garandsoftware.co.uk>
2018-01-15 11:23:52 +0000

    Removed StatType enum as it's going inside Statistic

--- FootballData/Statistics.cs
+++ FootballData/Statistics.cs
@@ -5,18 +5,5 @@ namespace FootballData
     public class Statistics
     {
         public List<Statistic> List { get; set; }
-
-        public enum StatType
-        {
-            Shots,
-            ShotsOnTarget,
-            HitWoodwork,
-            Corners,
-            FoulsCommitted,
-            Offsides,
-            YellowCards,
-            RedCards,
-            BookingPoints
-        }
     }
 }
