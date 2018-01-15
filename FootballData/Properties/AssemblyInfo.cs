c8ee62a5d7d02cf3ea09b4378d5ff6b0a143dc9f
Gary Anderson <gary@garandsoftware.co.uk>
2018-01-15 17:35:33 +0000

    Updated AssemblyInfo details for the library

--- FootballData/Properties/AssemblyInfo.cs
+++ FootballData/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FootballData")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Football-Data.co.uk Consumer")]
+[assembly: AssemblyDescription("A small utility to make accessing data from football-data.co.uk easier")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FootballData")]
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 // This is needed to allow the unit test project to have access to internal classes and methods
